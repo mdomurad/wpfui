938d7811a866a2f95864814a6a2b8c60906ece9d
md <marcindomurad@gmail.com>
2024-03-29 16:23:25 +0100

    dont rename namespaces in comments

--- src/Wpf.Ui/ren.ps1
+++ src/Wpf.Ui/ren.ps1
@@ -1,10 +1,20 @@
 param ($o, $n)
 $Utf8Encoding = New-Object System.Text.UTF8Encoding $True
 
-# Replace namespaces in .cs, .xaml, and .csproj files
+# Function to process each line, skipping lines that start with comments
+function Process-Line {
+    param ($line)
+    if ($line -notmatch '^\s*//') { # Check if the line does not start with //
+        return $line -replace $o, $n
+    } else {
+        return $line
+    }
+}
+
+# Replace namespaces in .cs, .xaml, and .csproj files, skipping comment lines
 Get-ChildItem * -Include *.cs, *.xaml, *.csproj -Recurse |
     Foreach-Object {
-        $content = ($_ | Get-Content) 
-        $content = $content -replace $o, $n
-        [IO.File]::WriteAllText($_.FullName, ($content -join "`r`n"), $Utf8Encoding)
+        $content = Get-Content $_.FullName
+        $newContent = $content | ForEach-Object { Process-Line $_ }
+        [IO.File]::WriteAllText($_.FullName, ($newContent -join "`r`n"), $Utf8Encoding)
     }
